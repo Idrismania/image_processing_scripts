1ff711180f84b8a40182e65bbe380327592d0ab2
IdrisI21 <idrisiritas21@gmail.com>
2024-01-29 15:08:52 +0100

    Add requirements and dlup script

---
+++ src/dlup_usage_example.py
@@ -0,0 +1,20 @@
+"""Some requirements to use dlup in Windows:
+IN ORDER: download binaries for openslide and pyvips and add them with os.add_dll_directory.
+pip install pyvips, openslide-python and dlup.
+Finally, import dlup only after importing openslide and pyvips through dll.
+Note that paths to dll directories have to be absolute paths
+
+Pyvips:     https://github.com/libvips/build-win64-mxe/releases/download/v8.15.1/vips-dev-w64-all-8.15.1.zip
+Openslide:  https://github.com/openslide/openslide-bin/releases/download/v20231011/openslide-win64-20231011.zip"""
+
+import os
+
+with os.add_dll_directory(r"C:\Users\idris\PycharmProjects\ChangeGamersScripts\backends\openslide\bin"):
+    import openslide
+
+with os.add_dll_directory(r"C:\Users\idris\PycharmProjects\ChangeGamersScripts\backends\pyvips\bin"):
+    import pyvips
+
+import dlup
+from pathlib import Path
+
