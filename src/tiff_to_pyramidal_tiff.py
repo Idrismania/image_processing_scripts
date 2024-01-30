49010bae6afbb8f3a2a6e52f46e010cc713e0114
IdrisI21 <idrisiritas21@gmail.com>
2024-01-30 13:15:38 +0100

    Add metadata to thumbnail and fix quotes

--- src/tiff_to_pyramidal_tiff.py
+++ src/tiff_to_pyramidal_tiff.py
@@ -9,7 +9,7 @@ from PIL import Image
 from tifffile import TiffWriter, COMPRESSION
 
 
-def main():
+def main() -> None:
 
     # Parse arguments
     parser = argparse.ArgumentParser(
@@ -35,26 +35,33 @@ def main():
         n_subifds = int(log2(max(image_array.shape)/256))
         
         metadata = {
-            'SignificantBits': 8,
-            'PhysicalSizeX': pixelsize,
-            'PhysicalSizeXUnit': 'µm',
-            'PhysicalSizeY': pixelsize,
-            'PhysicalSizeYUnit': 'µm',
+            "SignificantBits": 8,
+            "PhysicalSizeX": pixelsize,
+            "PhysicalSizeXUnit": "µm",
+            "PhysicalSizeY": pixelsize,
+            "PhysicalSizeYUnit": "µm",
         }
 
         # Adjust the number of axes depending on image type
         if len(image_array.shape) == 2:
-            metadata['axes'] = 'YX'
+            metadata["axes"] = "YX"
         elif len(image_array.shape) == 3:
-            metadata['axes'] = 'YXC'
+            metadata["axes"] = "YXC"
         else:
             raise ValueError("Unexpected image shape. Writer only supports single-channel and RGB images")
 
         options = dict(
             tile=(128, 128),
             # compression=COMPRESSION.ZSTD,
-            resolutionunit='CENTIMETER'
+            resolutionunit="CENTIMETER"
             )
+
+        if len(image_array.shape) == 2:
+            options["photometric"] = "minisblack"
+        elif len(image_array.shape) == 3:
+            options["photometric"] = "rgb"
+        else:
+            raise ValueError("Unexpected image shape. Writer only supports single-channel and RGB images")
 
         tif.write(
             image_array,
@@ -68,9 +75,9 @@ def main():
             
             mag = 2 ** (level + 1)
 
-            if metadata['axes'] == 'YX':
+            if metadata["axes"] == "YX":
                 pyramidal_slice_arg = image_array[::mag, ::mag]
-            elif metadata['axes'] == 'YXC':
+            elif metadata["axes"] == "YXC":
                 pyramidal_slice_arg = image_array[::mag, ::mag, :]
             else:
                 raise ValueError("Unsupported axes configuration in metadata")
@@ -83,8 +90,8 @@ def main():
                 **options
                 )
 
-        thumbnail = (image_array[::32, ::32] >> 2).astype('uint8')
-        tif.write(thumbnail, metadata={'Name': 'thumbnail'})
+        thumbnail = (image_array[::32, ::32] >> 2).astype("uint8")
+        tif.write(thumbnail, metadata={"Name": "thumbnail"}, **options)
 
 
 if __name__ == "__main__":
