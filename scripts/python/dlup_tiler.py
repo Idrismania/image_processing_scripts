9adcc702fb8b8bb85fa14bd98440a409c56a1ff0
Idris Iritas <i.iritas@nki.nl>
2024-05-25 21:59:17 +0200

    Adjust parameter setup and import order

--- scripts/python/dlup_tiler.py
+++ scripts/python/dlup_tiler.py
@@ -1,16 +1,30 @@
 """This script generates and saves an overview of your tiling settings."""
 
 import os
+from pathlib import Path
+
+# File paths of H&E and mask image to generate tiles from, as well as a reference to pyvips and openslide binaries paths necessay for import
+HE_FILE_PATH = Path(r"D:\Users\Horlings\ii_hh\FINAL_PROCESSED_IMAGE_DATA\d161fa98-0035-4c66-80f3-c02e11ff84ce_he.ome.tif")
+MASK_FILE_PATH = Path(r"D:\Users\Horlings\ii_hh\FINAL_PROCESSED_IMAGE_DATA\d161fa98-0035-4c66-80f3-c02e11ff84ce_mask.ome.tif")  # Mask
+
+PYVIPS_BIN_PATH = Path(r"D:\Users\Horlings\ii_hh\image_processing_scripts\backends\pyvips\bin\\")
+OPENSLIDE_BIN_PATH = Path(r"D:\Users\Horlings\ii_hh\image_processing_scripts\backends\openslide\bin\\")
+
+OUTPUT_PATH = Path(r"D:\Users\Horlings\ii_hh\image_processing_scripts\data\wsi")
+
+# Tiling parameters
+TARGET_MPP = 0.2425 # 0.5
+TILE_SIZE = (512, 512) # 512
 
 # Dynamically add pyvips bin path to PATH for ctypes libray to find it during import
 basepath = os.path.abspath(os.path.join(os.path.dirname( __file__ ), "..\.."))
 dllspath = os.path.join(basepath, "backends", "pyvips", "bin")
 os.environ['PATH'] = dllspath + os.pathsep + os.environ['PATH']
-print(dllspath)
-with os.add_dll_directory(r"D:\Users\Horlings\ii_hh\image_processing_scripts\backends\openslide\bin\\"):
+
+with os.add_dll_directory(OPENSLIDE_BIN_PATH):
     import openslide
 
-with os.add_dll_directory(r"D:\Users\Horlings\ii_hh\image_processing_scripts\backends\pyvips\bin\\"):
+with os.add_dll_directory(PYVIPS_BIN_PATH):
     import pyvips
 
 import dlup
@@ -19,23 +33,8 @@ import numpy as np
 import tifffile
 from dlup.data.dataset import TiledROIsSlideImageDataset
 from dlup.background import get_mask
-from pathlib import Path
 from PIL import Image, ImageDraw
 from tqdm import tqdm
-
-# File paths of H&E and mask image to generate tiles from, as well as a reference to pyvips binary paths necessay for import
-HE_FILE_PATH = Path(r"D:\Users\Horlings\ii_hh\FINAL_PROCESSED_IMAGE_DATA\d161fa98-0035-4c66-80f3-c02e11ff84ce_he.ome.tif")
-MASK_FILE_PATH = Path(r"D:\Users\Horlings\ii_hh\FINAL_PROCESSED_IMAGE_DATA\d161fa98-0035-4c66-80f3-c02e11ff84ce_mask.ome.tif")  # Mask
-PYVIPS_BIN_PATH = Path(r"D:\Users\Horlings\ii_hh\image_processing_scripts\backends\pyvips\bin\\")
-
-OUTPUT_PATH = Path(r"D:\Users\Horlings\ii_hh\image_processing_scripts\data\wsi")
-
-# Tiling parameters
-TARGET_MPP = 0.2425 # 0.5
-TILE_SIZE = (512, 512) # 512
-
-with os.add_dll_directory(PYVIPS_BIN_PATH):
-    import pyvips
 
 
 # Load H&E slide through dlup
